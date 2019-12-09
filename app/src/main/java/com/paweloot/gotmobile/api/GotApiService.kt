67cb3871c02ccabedd589f1298c49d861b9cc3d8
Paweł Banaś <paweloot@gmail.com>
2019-12-09 22:50:45 +0100

    Use Moshi for converting Json to model objects

--- app/src/main/java/com/paweloot/gotmobile/api/GotApiService.kt
+++ app/src/main/java/com/paweloot/gotmobile/api/GotApiService.kt
@@ -1,19 +1,27 @@
 package com.paweloot.gotmobile.api
 
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://url"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface GotApiService {
     @GET("mtnranges")
     fun getMtnRanges():
-            Call<String>
+            Call<List<MtnRange>>
 }
 
 object GotApi {
