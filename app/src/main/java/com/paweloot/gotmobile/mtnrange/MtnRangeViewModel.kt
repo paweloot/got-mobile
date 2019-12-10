0e15df0997778ba8fea1c883c2256d14ef41366b
Paweł Banaś <paweloot@gmail.com>
2019-12-10 14:29:26 +0100

    Reformat ViewModel to use repository

--- app/src/main/java/com/paweloot/gotmobile/mtnrange/MtnRangeViewModel.kt
+++ app/src/main/java/com/paweloot/gotmobile/mtnrange/MtnRangeViewModel.kt
@@ -1,39 +1,15 @@
 package com.paweloot.gotmobile.mtnrange
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.paweloot.gotmobile.api.GotApi
 import com.paweloot.gotmobile.model.MtnRange
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.paweloot.gotmobile.model.MtnRangeRepository
 
 class MtnRangeViewModel : ViewModel() {
 
-    private val _response = MutableLiveData<String>()
+    private val mtnRangeRepository: MtnRangeRepository = MtnRangeRepository()
 
-    val response: LiveData<String>
-        get() = _response
-
-    init {
-        getMtnRanges()
-    }
-
-    private fun getMtnRanges() {
-        GotApi.retrofitService.getMtnRanges().enqueue(object : Callback<List<MtnRange>> {
-            override fun onFailure(call: Call<List<MtnRange>>, t: Throwable) {
-                _response.value = "Failure" + t.message
-            }
-
-            override fun onResponse(
-                call: Call<List<MtnRange>>,
-                response: Response<List<MtnRange>>
-            ) {
-                _response.value = "Success " + response.body()?.size
-            }
-        })
-
-        _response.value = "Tatry i Podtatrze"
+    fun getMtnRanges(): LiveData<List<MtnRange>> {
+        return mtnRangeRepository.getLiveData()
     }
 }
