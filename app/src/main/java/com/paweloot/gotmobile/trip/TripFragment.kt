f51a56c170ea5d650c001b4d24bd40de15f4de07
Paweł Banaś <paweloot@gmail.com>
2019-12-11 20:31:11 +0100

    Bind MtnRange and MtnGroup to TripFragment

--- app/src/main/java/com/paweloot/gotmobile/trip/TripFragment.kt
+++ app/src/main/java/com/paweloot/gotmobile/trip/TripFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.paweloot.gotmobile.AppViewModel
 import com.paweloot.gotmobile.databinding.FragmentTripBinding
 
 class TripFragment : Fragment() {
 
     private lateinit var binding: FragmentTripBinding
+
+    private val appViewModel: AppViewModel by lazy {
+        ViewModelProviders.of(requireActivity()).get(AppViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +26,16 @@ class TripFragment : Fragment() {
         binding = FragmentTripBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
+        setUpBindingVariables()
+
         return binding.root
+    }
+
+    private fun setUpBindingVariables() {
+        val currMtnRange = appViewModel.mtnRange.value
+        val currMtnGroup = appViewModel.mtnGroup.value
+
+        binding.mtnRange = currMtnRange
+        binding.mtnGroup = currMtnGroup
     }
 }
