742ee93ac91f7b5ccd1519ac8c519f24ceb5812d
Paweł Banaś <paweloot@gmail.com>
2019-12-12 18:50:16 +0100

    Add Path model class

---
+++ app/src/main/java/com/paweloot/gotmobile/model/Path.kt
@@ -0,0 +1,12 @@
+package com.paweloot.gotmobile.model
+
+import com.squareup.moshi.Json
+
+data class Path(
+    @Json(name = "id_punkt_z") val idFrom: Int,
+    @Json(name = "id_punkt_do") val idTo: Int,
+    @Json(name = "dlugosc") val length: Int,
+    @Json(name = "roznica_wysokosci") val heightDiff: Int,
+    @Json(name = "punkty_got") val gotPoints: Int,
+    @Json(name = "punkty_got_powrot") val gotPointsReturn: Int
+)
