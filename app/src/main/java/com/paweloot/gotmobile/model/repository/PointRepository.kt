a1f6035a7b3be1bf97ff9f6b8c2a0da12308338f
Paweł Banaś <paweloot@gmail.com>
2020-01-14 14:18:32 +0100

    Create documentation for PointRepository

--- app/src/main/java/com/paweloot/gotmobile/model/repository/PointRepository.kt
+++ app/src/main/java/com/paweloot/gotmobile/model/repository/PointRepository.kt
@@ -12,11 +12,25 @@ import retrofit2.Response
 
 private const val TAG = "PointRepository"
 
+/**
+ * Repozytorium przechowujące oraz aktualizujące listę punktów pobraną z bazy danych,
+ * która może być obserwowana przez ViewModel.
+ */
 class PointRepository {
 
     private val _points = MutableLiveData<List<Point>>()
+    /**
+     * Lista pobranych punktów.
+     */
     val points: LiveData<List<Point>> = _points
 
+    /**
+     * Pobiera punkty z bazy danych dla wybranej grupy górskiej.
+     * Jeśli operacja zakończy się powodzeniem, lista przechowywana w klasie jest
+     * aktualizowana. W przeciwnym wypadku logowany jest błąd o niepowodzeniu.
+     *
+     * @param mtnGroup wybrana grupa górska
+     */
     fun fetchPoints(mtnGroup: MtnGroup) {
 
         RetrofitApi.gotApi.getPoints(mtnGroup.id).enqueue(object : Callback<List<Point>> {
@@ -30,6 +44,11 @@ class PointRepository {
         })
     }
 
+    /**
+     * Pobiera punkty z bazy danych, które są osiągalne z wybranego punktu.
+     * W przypadku powodzenia operacji, lista przechowywana w klasie jest aktualizowana.
+     * W p.p. logowany jest błąd o niepowodzeniu.
+     */
     fun filterPoints(pointFrom: Point) {
 
         RetrofitApi.gotApi.getFilteredPoints(pointFrom.id).enqueue(object : Callback<List<Point>> {
