38475baad665c9aa7e62681ce58a83d50dfc5ae4
Paweł Banaś <paweloot@gmail.com>
2020-02-05 12:09:13 +0100

    Replace OnClickHandler with custom click listener

--- app/src/main/java/com/paweloot/gotmobile/mtnrange/view/MtnRangeAdapter.kt
+++ app/src/main/java/com/paweloot/gotmobile/mtnrange/view/MtnRangeAdapter.kt
@@ -3,41 +3,45 @@ package com.paweloot.gotmobile.mtnrange.view
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.paweloot.gotmobile.AppViewModel
 import com.paweloot.gotmobile.databinding.ListItemMtnRangeBinding
 import com.paweloot.gotmobile.model.entity.MtnRange
 
-class MtnRangeAdapter(
-    private val appViewModel: AppViewModel,
-    private val mtnRanges: List<MtnRange>
-) :
+class MtnRangeAdapter(private val clickListener: OnMtnRangeClickedListener) :
     RecyclerView.Adapter<MtnRangeAdapter.MtnRangeHolder>() {
+
+    private var mtnRanges: List<MtnRange> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MtnRangeHolder {
 
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ListItemMtnRangeBinding.inflate(inflater, parent, false)
+        val binding =
+            ListItemMtnRangeBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
 
         return MtnRangeHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MtnRangeHolder, position: Int) {
-        holder.binding.mtnRange = mtnRanges[position]
+        val mtnRange = mtnRanges[position]
 
-        val navDirection =
-            MtnRangeFragmentDirections.openMtnGroupFragment()
-        holder.binding.handler =
-            OnClickHandler(
-                mtnRanges[position],
-                appViewModel,
-                navDirection
-            )
+        holder.binding.mtnRange = mtnRange
+        holder.binding.root.setOnClickListener {
+            clickListener.onMtnRangeClicked(mtnRange)
+        }
     }
 
     override fun getItemCount() = mtnRanges.size
 
+    fun setMtnRanges(mtnRanges: List<MtnRange>) {
+        this.mtnRanges = mtnRanges
+        notifyDataSetChanged()
+    }
+
     inner class MtnRangeHolder(val binding: ListItemMtnRangeBinding) :
         RecyclerView.ViewHolder(binding.root) {
+    }
 
+    interface OnMtnRangeClickedListener {
+        fun onMtnRangeClicked(mtnRange: MtnRange)
     }
 }
--- app/src/main/java/com/paweloot/gotmobile/mtnrange/view/MtnRangeFragment.kt
+++ app/src/main/java/com/paweloot/gotmobile/mtnrange/view/MtnRangeFragment.kt
@@ -8,24 +8,23 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.paweloot.gotmobile.AppViewModel
 import com.paweloot.gotmobile.MainActivity
 import com.paweloot.gotmobile.R
 import com.paweloot.gotmobile.databinding.FragmentMtnRangeBinding
+import com.paweloot.gotmobile.model.entity.MtnRange
 import com.paweloot.gotmobile.mtnrange.MtnRangeViewModel
 
+class MtnRangeFragment : Fragment(), MtnRangeAdapter.OnMtnRangeClickedListener {
 
-class MtnRangeFragment : Fragment() {
-
-    private val viewModel: MtnRangeViewModel by lazy {
-        ViewModelProviders.of(this).get(MtnRangeViewModel::class.java)
-    }
-
+    private lateinit var viewModel: MtnRangeViewModel
     private lateinit var appViewModel: AppViewModel
 
     private lateinit var binding: FragmentMtnRangeBinding
+    private lateinit var mtnRangeList: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,14 +32,55 @@ class MtnRangeFragment : Fragment() {
     ): View? {
 
         binding = FragmentMtnRangeBinding.inflate(inflater)
-
         binding.lifecycleOwner = this
-        binding.mtnRangeList.layoutManager = LinearLayoutManager(context)
 
-        appViewModel = ViewModelProviders.of(requireActivity()).get(AppViewModel::class.java)
+        mtnRangeList = binding.mtnRangeList
+
+        mtnRangeList.layoutManager = LinearLayoutManager(context)
+        mtnRangeList.adapter = MtnRangeAdapter(this)
+
+        viewModel = ViewModelProvider(this).get(MtnRangeViewModel::class.java)
+        appViewModel = ViewModelProvider(requireActivity()).get(AppViewModel::class.java)
 
         observeMtnRanges()
+        handleBackPressing()
 
+        return binding.root
+    }
+
+    override fun onMtnRangeClicked(mtnRange: MtnRange) {
+        appViewModel.mtnRange.value = mtnRange
+        appViewModel.newDestination.value =
+            MtnRangeFragmentDirections.openMtnGroupFragment().actionId
+    }
+
+    private fun observeMtnRanges() {
+        viewModel.mtnRanges.observe(viewLifecycleOwner, Observer { mtnRanges ->
+
+            if (mtnRanges == null) {
+                showConnectionErrorDialog()
+            } else {
+                binding.apply {
+                    progressCircular.visibility = View.GONE
+                    mtnRangeList.visibility = View.VISIBLE
+
+                    (mtnRangeList.adapter as MtnRangeAdapter).setMtnRanges(mtnRanges)
+                }
+            }
+        })
+    }
+
+    private fun showConnectionErrorDialog() {
+        val alertDialog = AlertDialog.Builder(context)
+            .setMessage(getString(R.string.alert_dialog_failed_to_connect))
+            .setPositiveButton(
+                R.string.button_continue
+            ) { dialogInterface, i -> dialogInterface.dismiss() }
+
+        alertDialog.show()
+    }
+
+    private fun handleBackPressing() {
         requireActivity().onBackPressedDispatcher.addCallback(
             this, object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
@@ -48,29 +88,5 @@ class MtnRangeFragment : Fragment() {
                     activity.showLogOutAlert()
                 }
             })
-
-        return binding.root
-    }
-
-    private fun observeMtnRanges() {
-        viewModel.mtnRanges.observe(this, Observer { mtnRanges ->
-
-            if (mtnRanges == null) {
-                val alertDialog = AlertDialog.Builder(context)
-                    .setMessage(getString(R.string.alert_dialog_failed_to_connect))
-                    .setPositiveButton(
-                        R.string.button_continue
-                    ) { dialogInterface, i -> dialogInterface.dismiss() }
-
-                alertDialog.show()
-            } else {
-                binding.apply {
-                    progressCircular.visibility = View.GONE
-                    mtnRangeList.visibility = View.VISIBLE
-
-                    mtnRangeList.adapter = MtnRangeAdapter(appViewModel, mtnRanges)
-                }
-            }
-        })
     }
 }
