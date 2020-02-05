64c2a17d706c4d9f80cf26ed5a17ca4f03ae9f69
Paweł Banaś <paweloot@gmail.com>
2020-02-05 12:26:56 +0100

    Replace OnClickHandler with custom click listener

--- app/src/main/java/com/paweloot/gotmobile/mtngroup/view/MtnGroupAdapter.kt
+++ app/src/main/java/com/paweloot/gotmobile/mtngroup/view/MtnGroupAdapter.kt
@@ -3,42 +3,45 @@ package com.paweloot.gotmobile.mtngroup.view
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.paweloot.gotmobile.AppViewModel
 import com.paweloot.gotmobile.databinding.ListItemMtnGroupBinding
 import com.paweloot.gotmobile.model.entity.MtnGroup
 
-class MtnGroupAdapter(
-    private val appViewModel: AppViewModel,
-    private val mtnGroups: List<MtnGroup>
-) :
+class MtnGroupAdapter(private val clickListener: OnMtnGroupClickListener) :
     RecyclerView.Adapter<MtnGroupAdapter.MtnGroupHolder>() {
+
+    private var mtnGroups: List<MtnGroup> = emptyList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MtnGroupHolder {
 
-        val inflater = LayoutInflater.from(parent.context)
         val binding =
-            ListItemMtnGroupBinding.inflate(inflater, parent, false)
+            ListItemMtnGroupBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
 
         return MtnGroupHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MtnGroupHolder, position: Int) {
-        holder.binding.mtnGroup = mtnGroups[position]
+        val mtnGroup = mtnGroups[position]
 
-        val navDirection =
-            MtnGroupFragmentDirections.openTripFragment()
-        holder.binding.handler =
-            OnClickHandler(
-                mtnGroups[position],
-                appViewModel,
-                navDirection
-            )
+        holder.binding.mtnGroup = mtnGroup
+        holder.binding.root.setOnClickListener {
+            clickListener.onMtnGroupClicked(mtnGroup)
+        }
     }
 
     override fun getItemCount() = mtnGroups.size
 
+    fun setMtnGroups(mtnGroups: List<MtnGroup>) {
+        this.mtnGroups = mtnGroups
+        notifyDataSetChanged()
+    }
+
     inner class MtnGroupHolder(val binding: ListItemMtnGroupBinding) :
         RecyclerView.ViewHolder(binding.root) {
+    }
 
+    interface OnMtnGroupClickListener {
+        fun onMtnGroupClicked(mtnGroup: MtnGroup)
     }
 }
--- app/src/main/java/com/paweloot/gotmobile/mtngroup/view/MtnGroupFragment.kt
+++ app/src/main/java/com/paweloot/gotmobile/mtngroup/view/MtnGroupFragment.kt
@@ -7,37 +7,40 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.paweloot.gotmobile.AppViewModel
 import com.paweloot.gotmobile.R
 import com.paweloot.gotmobile.databinding.FragmentMtnGroupBinding
+import com.paweloot.gotmobile.model.entity.MtnGroup
 import com.paweloot.gotmobile.mtngroup.MtnGroupViewModel
 
 
-class MtnGroupFragment : Fragment() {
+class MtnGroupFragment : Fragment(), MtnGroupAdapter.OnMtnGroupClickListener {
 
-    private val viewModel: MtnGroupViewModel by lazy {
-        ViewModelProviders.of(this).get(MtnGroupViewModel::class.java)
-    }
-
-    private val appViewModel: AppViewModel by lazy {
-        ViewModelProviders.of(requireActivity()).get(AppViewModel::class.java)
-    }
+    private lateinit var viewModel: MtnGroupViewModel
+    private lateinit var appViewModel: AppViewModel
 
     private lateinit var binding: FragmentMtnGroupBinding
+    private lateinit var mtnGroupList: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        viewModel = ViewModelProvider(this).get(MtnGroupViewModel::class.java)
+        appViewModel = ViewModelProvider(requireActivity()).get(AppViewModel::class.java)
+
         binding = FragmentMtnGroupBinding.inflate(inflater)
-
         binding.lifecycleOwner = this
-        binding.mtnGroupList.layoutManager = LinearLayoutManager(context)
-
         binding.mtnRange = appViewModel.mtnRange.value
+
+        mtnGroupList = binding.mtnGroupList
+
+        mtnGroupList.layoutManager = LinearLayoutManager(context)
+        mtnGroupList.adapter = MtnGroupAdapter(this)
 
         observeMtnGroups()
 
@@ -46,28 +49,33 @@ class MtnGroupFragment : Fragment() {
         return binding.root
     }
 
+    override fun onMtnGroupClicked(mtnGroup: MtnGroup) {
+        appViewModel.mtnGroup.value = mtnGroup
+        appViewModel.newDestination.value =
+            MtnGroupFragmentDirections.openTripFragment().actionId
+    }
+
     private fun observeMtnGroups() {
-        viewModel.mtnGroups.observe(this, Observer { mtnGroups ->
+        viewModel.mtnGroups.observe(viewLifecycleOwner, Observer { mtnGroups ->
 
             if (mtnGroups == null) {
-                val alertDialog = AlertDialog.Builder(context)
-                    .setMessage(getString(R.string.alert_dialog_failed_to_connect))
-                    .setPositiveButton(
-                        R.string.button_continue
-                    ) { dialogInterface, i -> dialogInterface.dismiss() }
-
-                alertDialog.show()
+                showConnectionErrorDialog()
             } else {
                 binding.progressCircular.visibility = View.GONE
                 binding.mtnGroupList.visibility = View.VISIBLE
 
-                binding.mtnGroupList.adapter =
-                    MtnGroupAdapter(
-                        appViewModel,
-                        mtnGroups
-                    )
+                (mtnGroupList.adapter as MtnGroupAdapter).setMtnGroups(mtnGroups)
             }
         })
     }
 
+    private fun showConnectionErrorDialog() {
+        val alertDialog = AlertDialog.Builder(context)
+            .setMessage(getString(R.string.alert_dialog_failed_to_connect))
+            .setPositiveButton(
+                R.string.button_continue
+            ) { dialogInterface, i -> dialogInterface.dismiss() }
+
+        alertDialog.show()
+    }
 }
